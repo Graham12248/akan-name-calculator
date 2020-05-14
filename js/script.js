1a8c7f232954d7fff4a5e59df11c9e5dcd06a9fd
Graham1228 <grotieno12@gmail.com>
2020-05-14 23:55:08 +0300

    added some codes on my js

--- js/script.js
+++ js/script.js
@@ -27,3 +27,33 @@ function getAkanName () {
       return true;
     }
   }
+  function dayValidator () {
+    if (monthOfBirth === 2 && Number(yearOfBirth)%4 === 0) {
+      if (dayOfBirth > 28 || dayOfBirth < 1) {
+        return false;
+      } else if (monthOfBirth === 2 && dayOfBirth > 29) {
+        return false;
+      } else if (monthOfBirth === 2 && dayOfBirth < 1) {
+        return false;
+      } else {
+        return true;
+      }
+    } else if (dayOfBirth < 1 || dayOfBirth > 31){
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  //validation variables
+  let monthValid = monthValidator();
+  let dayValid = dayValidator();
+
+  //formula to determine day of birth (Sunday = 1, Monday = 2)etc..
+  let dayOfWeekNumber = Math.floor((((Number(yearOfBirth.slice(0,2))/4)-2*Number(yearOfBirth.slice(0,2))-1)+
+          ((5*Number(yearOfBirth.slice(2,4))/4))+((26*(monthOfBirth+1)/10))+dayOfBirth)%7);
+
+  //creating arrays of Akan names for males and females
+  let maleAkanNames = [
+    "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Kwaku", "Yaw", "Kofi", "Kwame"
+  ];
