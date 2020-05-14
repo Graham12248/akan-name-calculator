8342f4b494c3de5d5f8c83254a5cd7e20d97c310
Graham1228 <grotieno12@gmail.com>
2020-05-15 02:22:22 +0300

    walk through on js css and index.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 function getAkanName () {
   let yearOfBirth = document.getElementById("year-input").value;
   let monthOfBirth = Number(document.getElementById("month-input").value);
@@ -29,38 +27,37 @@ function getAkanName () {
     }
   }
 
-  function dayValidator () {
-    if (monthOfBirth === 2 && Number(yearOfBirth)%4 === 0) {
-      if (dayOfBirth > 28 || dayOfBirth < 1) {
-        return false;
-      } else if (monthOfBirth === 2 && dayOfBirth > 29) {
-        return false;
-      } else if (monthOfBirth === 2 && dayOfBirth < 1) {
-        return false;
-      } else {
-        return true;
-      }
-    } else if (dayOfBirth < 1 || dayOfBirth > 31){
-      return false;
-    } else {
-      return true;
-    }
-  }
+function dayValidator () {
+ if (monthOfBirth === 2 && Number(yearOfBirth)%4 === 0) {
+   if (dayOfBirth > 28 || dayOfBirth < 1) {
+     return false;
+   } else if (monthOfBirth === 2 && dayOfBirth > 29) {
+     return false;
+   } else if (monthOfBirth === 2 && dayOfBirth < 1) {
+     return false;
+   } else {
+     return true;
+   }
+ } else if (dayOfBirth < 1 || dayOfBirth > 31){
+   return false;
+ } else {
+   return true;
+ }
+}
 
-  //validation variables
-  let monthValid = monthValidator();
-  let dayValid = dayValidator();
+//validation variables
+let monthValid = monthValidator();
+let dayValid = dayValidator();
 
-  //formula to determine day of birth (Sunday = 1, Monday = 2)etc..
-  let dayOfWeekNumber = Math.floor((((Number(yearOfBirth.slice(0,2))/4)-2*Number(yearOfBirth.slice(0,2))-1)+
-          ((5*Number(yearOfBirth.slice(2,4))/4))+((26*(monthOfBirth+1)/10))+dayOfBirth)%7);
+//formula to determine day of birth (Sunday = 1, Monday = 2)etc..
+let dayOfWeekNumber = Math.floor((((Number(yearOfBirth.slice(0,2))/4)-2*Number(yearOfBirth.slice(0,2))-1)+
+       ((5*Number(yearOfBirth.slice(2,4))/4))+((26*(monthOfBirth+1)/10))+dayOfBirth)%7);
 
-  //creating arrays of Akan names for males and females
-  let maleAkanNames = [
-    "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Kwaku", "Yaw", "Kofi", "Kwame"
-  ];
-
-  let femaleAkanNames = [
+//creating arrays of Akan names for males and females
+let maleAkanNames = [
+ "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Kwaku", "Yaw", "Kofi", "Kwame"
+];
+let femaleAkanNames = [
     "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
   ];
 
@@ -118,7 +115,7 @@ function getAkanName () {
         document.getElementById('display-name').textContent = "Here is your Akan name: ";
         return false;
         break;
-      case 2:
+        case 2:
         document.getElementById('result').textContent = "You were born on a Monday: your Akan name is " + femaleAkanNames[1];
         document.getElementById('display-name').textContent = "Here is your Akan name: ";
         return false;
@@ -149,7 +146,7 @@ function getAkanName () {
         document.getElementById('display-name').textContent = "Here is your Akan name: ";
         return false;
         break;
-      case 0:
+        case 0:
         document.getElementById('result').textContent = "You were born on a Sunday: your Akan name is " + femaleAkanNames[0];
         document.getElementById('display-name').textContent = "Here is your Akan name: ";
         return false;
@@ -160,4 +157,4 @@ function getAkanName () {
     } else{
       alert("You entered invalid month or day");
     }
- 
+  }
